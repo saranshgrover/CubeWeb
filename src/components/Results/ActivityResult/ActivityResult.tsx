adbc111d9a889d513dc9620b81903d3f049fd71c
saranshgrover <ycubiksrube@gmail.com>
2019-12-07 18:57:30 -0500

    Add Result Routing 🎉

---
+++ src/components/Results/ActivityResult/ActivityResult.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+interface Props {}
+
+export default function ActivityResult({}: Props): React.ReactElement {
+  return <div></div>
+}
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Competition from './Competition/Competition'
+import ActivityResult from './ActivityResult/ActivityResult'
 
 interface Props {}
 
 function Results({}: Props): React.ReactElement {
-  return <div></div>
+  return (
+    <>
+      <Route
+        path='/competitions/:competitionId/:activityCode'
+        compoenent={ActivityResult}
+      />
+      <Route path='/competitions/:competitionId' component={Competition} />
+    </>
+  )
 }
 
 export default Results
